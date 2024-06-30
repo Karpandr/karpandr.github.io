6980970dc67037636946d139d2b8f9beeed75d3d
Andrey Karpiuk <60291422+Karpandr@users.noreply.github.com>
2024-06-30 18:54:38 +0500

    Added App, Header, Layout, Logo components (#2)

---
+++ src/app/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import shoppingCartLogo from './shopping-cart.svg';
+
+const Logo = () => {
+  return <img src={shoppingCartLogo} className="App-logo" alt="logo" />;
+};
+
+export default Logo;
