c869e8915d1a97884fb211a155e02a6aa4c45af4
Andrey Karpiuk <akarpiuk@getnerdio.com>
2024-06-30 18:48:54 +0500

    Added App, Header, Layout, Logo components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from './Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Меня зовут Андрей, у меня 4 года опыта разработки на .NET, 1 год разработки на jQuery и 1 год разработки на
-          React и Redux.
-        </p>
-        <p>
-          Хочу улучшить свои знания по React, узнать актуальный стек, используемый во фронтенд разработке совместно с
-          React и научиться его использовать. Также хочу узнать новые паттерны проектирования.
-        </p>
-      </header>
-    </div>
-  );
+  return <Layout />;
 }
 
 export default App;
---
+++ src/app/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from 'src/app/Logo';
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <Logo />
+      <h3>Super store</h3>
+    </header>
+  );
+};
+
+export default Header;
