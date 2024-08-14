f14dd116b567dcdf7b7649bfd716c55515be4707
dan83g <dan83mail@gmail.com>
2024-08-15 00:04:28 +0300

    added ThemeProvider, LocalizationProvider, Modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Layout from '../pages/Layout/Layout';
+import { LocalizationProvider } from 'src/widgets/LocalizationSwitcher/LocalizationProvider';
+import { ThemeProvider } from 'src/widgets/ThemeSwitcher/ThemeProvider';
 
 function App() {
-  return <Layout />;
+  return (
+    <>
+      <LocalizationProvider />
+      <ThemeProvider>
+        <div className="App">
+          <Layout />
+        </div>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Logo from '../../entities/Logo/Logo';
 import s from './Header.module.sass';
+import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
+import { LocalizationSwitcher } from '../LocalizationSwitcher/LocalizationSwitcher';
 
 const Header = () => {
   return (
     <header className={s['header']}>
       <Logo />
       <h3>Super store</h3>
+      <div className={s['flexgrowdiv']} />
+      <ThemeSwitcher />
+      <LocalizationSwitcher />
     </header>
   );
 };
