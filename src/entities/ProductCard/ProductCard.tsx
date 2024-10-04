c044c8be6b4509d187e2b0f7193d962b0d22ff6c
dan83g <dan83mail@gmail.com>
2024-10-05 00:22:24 +0300

    added ProductList, ModalPortal

--- src/entities/ProductCard/ProductCard.tsx
+++ src/entities/ProductCard/ProductCard.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import s from './ProductCard.module.sass';
 import Basket from '../Basket/Basket';
+import { Category } from './types';
+import cn from 'clsx';
 
 interface ProductCardProps {
+  id: string;
   price: number;
   image: string;
-  title: string;
+  name: string;
   description: string;
+  category: Category;
+  observerClassName: string;
 }
 
-const ProductCard = ({ price, image, title, description }: ProductCardProps) => {
+const ProductCard = ({ observerClassName, price, image, name, description }: ProductCardProps) => {
   return (
-    <div className={s['product-card']}>
-      <img src={image} alt={title} className={s['product-card__image']} />
-      <p>{title}</p>
+    <div className={cn(observerClassName, s['product-card'])}>
+      <img src={image} alt={name} className={s['product-card__image']} />
+      <p>{name}</p>
       <p className={s['product-card__description']}>{description}</p>
       <p>{price}$</p>
       <Basket count={0} />
